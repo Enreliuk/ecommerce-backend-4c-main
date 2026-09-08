8879903dc8532a6dd68817eacb43a51b8e596c7a
Guilherme Santos <119358054+Enreliuk@users.noreply.github.com>
2026-09-08 23:56:52 +0000

    Criação de Repositórios

---
+++ ecommerce/src/test/java/br/edu/unifio/ecommerce/repositorios/ProdutoRepositorioTests.java
@@ -0,0 +1,30 @@
+package br.edu.unifio.ecommerce.repositorios;
+
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import br.edu.unifio.ecommerce.entidades.Categoria;
+import br.edu.unifio.ecommerce.entidades.Produto;
+
+@SpringBootTest
+public class ProdutoRepositorioTests {
+    @Autowired 
+    private CategoriaRepositorio categoriaRepositorio;
+
+    @Autowired 
+    private ProdutoRepositorio produtoRepositorio;
+
+    @Test  
+    public void deveSalvarUmProdutoNovo () {
+        var produto = new Produto ();
+        produto.setNome("Notebook Lenovo Legion 5i");
+        produto.setDescricao("Processador I7, Armazenamento SSD 1TB, Memória 16BG");
+        produto.setPreco(new BigDecimal("12570.30"));
+        produto.setEstoque(Short.parseShort("10"));
+        
+        var categoria = categoriaRepositorio.findById(Short.parseShort("1")).orElseThrow();
+    }
+}
