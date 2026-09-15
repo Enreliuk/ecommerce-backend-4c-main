43505915762d2bf36733259eb2eeabeaabd70922
Guilherme Santos <119358054+Enreliuk@users.noreply.github.com>
2026-09-15 23:25:00 +0000

    Mais Testes

--- ecommerce/src/test/java/br/edu/unifio/ecommerce/repositorios/ProdutoRepositorioTests.java
+++ ecommerce/src/test/java/br/edu/unifio/ecommerce/repositorios/ProdutoRepositorioTests.java
@@ -8,6 +8,8 @@ import java.math.BigDecimal;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Sort;
+import java.util.List;
 
 import br.edu.unifio.ecommerce.entidades.Produto;
 
@@ -36,5 +38,37 @@ public class ProdutoRepositorioTests {
 
         assertNotNull(produto.getId());
         assertEquals(6, produto.getId());
+    }
+
+    @Test
+    public void deveBuscarUmProdutoPorId () {
+        Produto produto = produtoRepositorio.findById(Integer.parseInt("3")).orElseThrow();
+
+        assertNotNull(produto);
+        assertEquals("JBL Partybox Club 120", produto.getNome());
+    }
+
+    @Test
+public void deveBuscarTodosOsProdutos () {
+	List<Produto> produto = produtoRepositorio.findAll(Sort.by("nome"));
+
+    assertEquals(6, produto.size());
+    assertEquals("Código Limpo", produto.get(0).getNome());
+    assertEquals("Razer Basilisk V3", produto.get(5).getNome());
+    }
+
+    @Test
+    public void deveExcluirUmProdutoPorId () {
+        var produto = new Produto ();
+        produto.setNome("Nome Teste");
+        produto.setDescricao("Descrição Teste");
+        produto.setPreco(new BigDecimal("67.67"));
+        produto.setEstoque(Short.parseShort("67"));
+        
+        var categoria = categoriaRepositorio.findById(Short.parseShort("1")).orElseThrow();
+        produto.setCategoria(categoria);
+
+        produtoRepositorio.save(produto);
+        produtoRepositorio.deleteById(produto.getId());
     }
 }
